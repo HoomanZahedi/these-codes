9144668f6c72e9eb060d8a01b992482ea1ba8353
Hooman <h00man.fb.77@gmail.com>
2020-06-21 00:06:37 +0430

    first

---
+++ These_Code/OD With YOLO/darkNet.py
@@ -0,0 +1,142 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+from IPython.display import display
+from seaborn import color_palette
+import cv2
+
+
+_BATCH_NORM_DECAY = 0.9
+_BATCH_NORM_EPSILON = 1e-05
+_LEAKY_RELU = 0.1
+_ANCHORS = [(10, 13), (16, 30), (33, 23),
+            (30, 61), (62, 45), (59, 119),
+            (116, 90), (156, 198), (373, 326)]
+_MODEL_SIZE = (416, 416)
+
+
+def batch_norm(inputs, training, data_format):
+    """Performs a batch normalization using a standard set of parameters."""
+    return tf.layers.batch_normalization(
+        inputs=inputs, axis=1 if data_format == 'channels_first' else 3,
+        momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON,
+        scale=True, training=training)
+
+
+def fixed_padding(inputs, kernel_size, data_format):
+    """ResNet implementation of fixed padding.
+
+    Pads the input along the spatial dimensions independently of input size.
+
+    Args:
+        inputs: Tensor input to be padded.
+        kernel_size: The kernel to be used in the conv2d or max_pool2d.
+        data_format: The input format.
+    Returns:
+        A tensor with the same format as the input.
+    """
+    pad_total = kernel_size - 1
+    pad_beg = pad_total // 2
+    pad_end = pad_total - pad_beg
+
+    if data_format == 'channels_first':
+        padded_inputs = tf.pad(inputs, [[0, 0], [0, 0],
+                                        [pad_beg, pad_end],
+                                        [pad_beg, pad_end]])
+    else:
+        padded_inputs = tf.pad(inputs, [[0, 0], [pad_beg, pad_end],
+                                        [pad_beg, pad_end], [0, 0]])
+    return padded_inputs
+
+
+def conv2d_fixed_padding(inputs, filters, kernel_size, data_format, strides=1):
+    """Strided 2-D convolution with explicit padding."""
+    if strides > 1:
+        inputs = fixed_padding(inputs, kernel_size, data_format)
+
+    return tf.layers.conv2d(
+        inputs=inputs, filters=filters, kernel_size=kernel_size,
+        strides=strides, padding=('SAME' if strides == 1 else 'VALID'),
+        use_bias=False, data_format=data_format)
+    
+    def darknet53_residual_block(inputs, filters, training, data_format,
+                             strides=1):
+    """Creates a residual block for Darknet."""
+    shortcut = inputs
+
+    inputs = conv2d_fixed_padding(
+        inputs, filters=filters, kernel_size=1, strides=strides,
+        data_format=data_format)
+    inputs = batch_norm(inputs, training=training, data_format=data_format)
+    inputs = tf.nn.leaky_relu(inputs, alpha=_LEAKY_RELU)
+
+    inputs = conv2d_fixed_padding(
+        inputs, filters=2 * filters, kernel_size=3, strides=strides,
+        data_format=data_format)
+    inputs = batch_norm(inputs, training=training, data_format=data_format)
+    inputs = tf.nn.leaky_relu(inputs, alpha=_LEAKY_RELU)
+
+    inputs += shortcut
+
+    return inputs
+
+
+def darknet53(inputs, training, data_format):
+    """Creates Darknet53 model for feature extraction."""
+    inputs = conv2d_fixed_padding(inputs, filters=32, kernel_size=3,
+                                  data_format=data_format)
+    inputs = batch_norm(inputs, training=training, data_format=data_format)
+    inputs = tf.nn.leaky_relu(inputs, alpha=_LEAKY_RELU)
+    inputs = conv2d_fixed_padding(inputs, filters=64, kernel_size=3,
+                                  strides=2, data_format=data_format)
+    inputs = batch_norm(inputs, training=training, data_format=data_format)
+    inputs = tf.nn.leaky_relu(inputs, alpha=_LEAKY_RELU)
+
+    inputs = darknet53_residual_block(inputs, filters=32, training=training,
+                                      data_format=data_format)
+
+    inputs = conv2d_fixed_padding(inputs, filters=128, kernel_size=3,
+                                  strides=2, data_format=data_format)
+    inputs = batch_norm(inputs, training=training, data_format=data_format)
+    inputs = tf.nn.leaky_relu(inputs, alpha=_LEAKY_RELU)
+
+    for _ in range(2):
+        inputs = darknet53_residual_block(inputs, filters=64,
+                                          training=training,
+                                          data_format=data_format)
+
+    inputs = conv2d_fixed_padding(inputs, filters=256, kernel_size=3,
+                                  strides=2, data_format=data_format)
+    inputs = batch_norm(inputs, training=training, data_format=data_format)
+    inputs = tf.nn.leaky_relu(inputs, alpha=_LEAKY_RELU)
+
+    for _ in range(8):
+        inputs = darknet53_residual_block(inputs, filters=128,
+                                          training=training,
+                                          data_format=data_format)
+
+    route1 = inputs
+
+    inputs = conv2d_fixed_padding(inputs, filters=512, kernel_size=3,
+                                  strides=2, data_format=data_format)
+    inputs = batch_norm(inputs, training=training, data_format=data_format)
+    inputs = tf.nn.leaky_relu(inputs, alpha=_LEAKY_RELU)
+
+    for _ in range(8):
+        inputs = darknet53_residual_block(inputs, filters=256,
+                                          training=training,
+                                          data_format=data_format)
+
+    route2 = inputs
+
+    inputs = conv2d_fixed_padding(inputs, filters=1024, kernel_size=3,
+                                  strides=2, data_format=data_format)
+    inputs = batch_norm(inputs, training=training, data_format=data_format)
+    inputs = tf.nn.leaky_relu(inputs, alpha=_LEAKY_RELU)
+
+    for _ in range(4):
+        inputs = darknet53_residual_block(inputs, filters=512,
+                                          training=training,
+                                          data_format=data_format)
+
+    return route1, route2, inputs
